6b6c0a8e4129fcc236150102b6a2f29e55e28940
Light326 <hikka.1357@gmail.com>
2024-01-09 16:35:31 +0900

    update informationEstimator.estimate

--- s4/B233367/TestCase.java
+++ s4/B233367/TestCase.java
@@ -128,9 +128,40 @@ public class TestCase {
 	    myObject.setTarget("00".getBytes());
 	    value = myObject.estimation();
 	    assert (value > 3.9999) && (4.0001 >value): "IQ for 00 in 3210321001230123 should be 3.0. But it returns "+value;
+
+
+		// Write your additional test cases here
+
+		// Test Case 1: Target is not set
+		myObject = new InformationEstimator();
+		myObject.setSpace("3210321001230123".getBytes());
+		value = myObject.estimation();
+	    assert (value > 0.0 - 0.0001) && (0.0 + 0.0001 >value): "IQ for 00 in 3210321001230123 should be 0.0. But it returns "+value;
+
+		// Test Case 2: Target's length is zero
+		myObject = new InformationEstimator();
+		myObject.setSpace("3210321001230123".getBytes());
+		myObject.setTarget(new byte[0]);
+		value = myObject.estimation();
+	    assert (value > 0.0 - 0.0001) && (0.0 + 0.0001 >value): "IQ for length 0 target in 3210321001230123 should be 0.0. But it returns "+value;
+
+		// Test Case 3: Space is not set
+		myObject = new InformationEstimator();
+		myObject.setTarget("0".getBytes());
+		value = myObject.estimation();
+	    assert (value == Double.MAX_VALUE): "IQ for 0 in empty set should be Double.MAX_VALUE. But it returns "+value;
+
+		// Test Case 4: Target not present in Space
+		myObject = new InformationEstimator();
+		myObject.setSpace("3210321001230123".getBytes());
+		myObject.setTarget("X".getBytes());
+		value = myObject.estimation();
+	    assert (value == Double.MAX_VALUE): "IQ for X in 3210321001230123 should be Double.MAX_VALUE. But it returns "+value;
+
 	}
 	catch(Exception e) {
 	    System.out.println("Exception occurred in InformationEstimator Object");
+		e.printStackTrace();
 	    success = false;
 	}
         if(success) { System.out.println("TestCase OK"); } 
